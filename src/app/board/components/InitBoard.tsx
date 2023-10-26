6dc46bbbbf06ee9e74713b86c1ea10d265b5f25c
Ajinkya Palaskar <palaskarajinkya22@gmail.com>
2023-10-26 18:44:32 +0530

    feat: add sign in button in init modal

--- src/app/board/components/InitBoard.tsx
+++ src/app/board/components/InitBoard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   Button,
@@ -8,7 +8,12 @@ import {
   FormLabel,
   Input,
   VStack,
+  Text,
 } from "@chakra-ui/react";
+
+import { useSession, signIn } from "next-auth/react";
+
+import { useBoundStore } from "@/zustand/store";
 
 type Props = {
   handleSave: (guestUserName: string) => void;
@@ -16,10 +21,23 @@ type Props = {
 
 const InitBoard = ({ handleSave }: Props) => {
   const [guestUserName, setGuestUserName] = useState("");
+  const setGuestUser = useBoundStore((state) => state.setGuestUser);
+
+  const { status } = useSession();
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setGuestUserName(e.target.value);
   };
+
+  const handleSignIn = () => {
+    signIn("google");
+  };
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      setGuestUser(null);
+    }
+  }, [status, setGuestUser]);
 
   return (
     <VStack gap="6">
@@ -34,23 +52,53 @@ const InitBoard = ({ handleSave }: Props) => {
         />
       </FormControl>
 
-      <Button
-        onClick={() => handleSave(guestUserName)}
-        w="full"
-        h="0"
-        py="5"
-        bg="darkPrimary"
-        border="1px"
-        borderColor="transparent"
-        color="white"
-        fontWeight="medium"
-        _hover={{
-          bg: "darkPrimary",
-          color: "white",
-        }}
-      >
-        Let&#39;s go!
-      </Button>
+      <VStack gap="2" w="full">
+        <Button
+          isDisabled={!guestUserName}
+          onClick={() => handleSave(guestUserName)}
+          w="full"
+          h="0"
+          py="5"
+          bg="darkPrimary"
+          border="1px"
+          borderColor="transparent"
+          color="white"
+          fontWeight="medium"
+          _hover={{
+            bg: "darkPrimary",
+            color: "white",
+          }}
+        >
+          Let&#39;s go!
+        </Button>
+
+        <Text
+          textAlign="center"
+          color="darkPrimary"
+          fontWeight="light"
+          fontSize="lg"
+        >
+          or
+        </Text>
+
+        <Button
+          onClick={handleSignIn}
+          w="full"
+          h="0"
+          py="5"
+          bg="transparent"
+          border="1px"
+          borderColor="darkPrimary"
+          color="darkPrimary"
+          fontWeight="medium"
+          _hover={{
+            bg: "darkPrimary",
+            color: "white",
+          }}
+        >
+          Sign in
+        </Button>
+      </VStack>
     </VStack>
   );
 };
